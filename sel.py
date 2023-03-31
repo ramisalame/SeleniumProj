787dd1020d28c706444d5161648d1a4402a4f59b
ramisalame <ramisalame@hotmail.com>
2023-03-30 22:08:45 -0400

    Create sel.py

---
+++ sel.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+s = Service(r"C:\Users\Rami Salame\OneDrive\Desktop\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+
+
+driver.get("https://www.google.com")
+
+search_box = driver.find_element_by_name("q")
+
+search_box.send_keys("Test")
+
+search_box.submit()
+
+
+
+
+
+
+
