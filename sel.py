f0a4ae69c5d08f6327bb1172d5c6dd44bc062f1e
ramisalame <ramisalame@hotmail.com>
2023-04-04 22:16:58 -0400

    Update sel.py

--- sel.py
+++ sel.py
@@ -1,15 +1,23 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 
 s = Service(r"C:\Users\Rami Salame\OneDrive\Desktop\chromedriver_win32\chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 
-driver.get("https://www.leagueoflegends.com/en-us/news/tags/patch-notes/")
+driver.get("https://www.leagueoflegends.com/en-us/news/game-updates/patch-13-7-notes/")
 
-text = driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/section/section[2]/div/div/div[3]/div/div/blockquote");
+element_xpath = "/html/body/div[2]/div[1]/div/section/section[2]/div/div/div[3]/div/div/blockquote"
+element = driver.find_element(By.XPATH, element_xpath)
 
+# Get the text of the element
+text = element.text
+
+# Print the text
 print(text)
+
 
 
 
